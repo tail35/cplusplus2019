c9e81ba071796c45ce2b277d493a610b3f1a923f
tail35 <ragingfire35@gmail.com>
2022-04-17 13:42:39 +0800

    add iotest

--- cplusplus2019.cpp
+++ cplusplus2019.cpp
@@ -1,9 +1,10 @@
 ﻿// cplusplus2019.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
-
+#include "common.h"
+#include "iotest.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    iotest();
 }
